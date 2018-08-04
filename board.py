ef6b6403e554840e020f1c472bc83e5413e2e827
Cem Eliguzel <cemeliguzel@gmail.com>
2018-08-04 19:42:53 +0200

    Adds first draft of Board implementation

---
+++ board.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+"""board.py: A package to represent Boggle board."""
+
+
+class Cell(object):
+
+    def __init__(self, ch, row, column):
+        self.ch = ch
+        self.row = row
+        self.column = column
+
+    def get_coordinates_of_neighbours(self):
+        if self.row > 0:
+            yield self.row - 1, self.column
+        if self.row < 3:
+            yield self.row + 1, self.column
+        if self.column > 0:
+            yield self.row, self.column - 1
+        if self.column < 3:
+            yield self.row, self.column + 1
+        if self.row > 0 and self.column > 0:
+            yield self.row - 1, self.column - 1
+        if self.row < 3 and self.column < 3:
+            yield self.row + 1, self.column + 1
+        if self.row < 3 and self.column > 0:
+            yield self.row + 1, self.column - 1
+        if self.row > 0 and self.column < 3:
+            yield self.row - 1, self.column + 1
+
+    def __eq__(self, o):
+        return self.row == o.row and self.column == o.column and self.ch == o.ch
+
+    def __str__(self):
+        return '({}, {}) - {}'.format(self.row, self.column, self.ch)
+
+    def __hash__(self):
+        return hash(str(self))
+
+
+class Board(object):
+
+    def __init__(self, config):
+        self.config = config
+
+    def get_cells_of_char(self, c):
+        for i, row in enumerate(self.config):
+            for j, v in enumerate(row):
+                if v == c:
+                    yield Cell(c, i, j)
+
+    def get_neighbour_cells(self, cell):
+        for x, y in cell.get_coordinates_of_neighbours():
+            yield Cell(self.config[x][y], x, y)
+
+    def get_neighbour_chars(self, c):
+        cells = self.get_cells_of_char(c)
+        for cell in cells:
+            neighbour_cells = self.get_neighbour_cells(cell)
+            for c in neighbour_cells:
+                yield c.ch
+
+    def contains(self, word):
+        starting_cells = list(self.get_cells_of_char(word[0]))
+        if not starting_cells:
+            return False
+        if len(word) == 1:
+            return True
+        for c in range(len(word)-1):
+            c, n = word[0], word[1]
+            if n not in set(self.get_neighbour_chars(c)):
+                return False
+        return True
---
+++ test_board.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+"""test_board.py: Tests for Board class."""
+
+from expects import expect, be, equal
+from board import Board, Cell
+
+
+def test_board_should_say_no_when_the_word_is_not_in_board():
+    # given
+    board = Board(['ADHG', 'PDFF', 'EKJU', 'FTGT'])
+    # when
+    res = board.contains('CAT')
+    # then
+    expect(res).to(be(False))
+
+#
+# def test_board_should_recognize_verticle_words():
+#     # given
+#     board = Board(['CDHG', 'ADFF', 'TKJU', 'FTGT'])
+#     # when
+#     res = board.contains('CAT')
+#     # then
+#     expect(res).to(be(True))
+
+
+def test_given_a_char_board_should_return_corresponding_cells():
+    # given
+    board = Board(['CDHG', 'ADFF', 'TKJU', 'FTGT'])
+    # when
+    res = board.get_cells_of_char('F')
+    # then
+    expect(list(res)).to(equal([Cell('F', 1, 2), Cell('F', 1, 3), Cell('F', 3, 0)]))
+
+
+def test_given_a_corner_cell_it_should_return_coordinates_of_correct_neighbours():
+    # given
+    cell = Cell('C', 0, 0)
+    # when
+    res = cell.get_coordinates_of_neighbours()
+    # then
+    expect(set(res)).to(equal(set([(1, 0), (1, 1), (0, 1)])))
+
+
+def test_given_a_edge_cell_it_should_return_coordinates_of_correct_neighbours():
+    # given
+    cell = Cell('C', 1, 0)
+    # when
+    res = cell.get_coordinates_of_neighbours()
+    # then
+    expect(set(res)).to(equal(set([(0, 0), (0, 1), (1, 1), (2, 0), (2, 1)])))
+
+
+def test_cells_with_same_values_should_be_equal():
+    # given
+    cell1 = Cell('C', 1, 0)
+    cell2 = Cell('C', 1, 0)
+    # when
+    res = cell1 == cell2
+    # then
+    expect(res).to(be(True))
+
+
+def test_given_a_corner_cell_board_should_return_neighbour_cells():
+    # given
+    board = Board(['CDHG', 'ADFF', 'TKJU', 'FTGT'])
+    cell = Cell('C', 0, 0)
+    # when
+    res = board.get_neighbour_cells(cell)
+    # then
+    expect(set(res)).to(equal(set([Cell('A', 1, 0), Cell('D', 1, 1), Cell('D', 0, 1)])))
+
+
+def test_board_should_verify_single_char_words():
+    # given
+    board = Board(['CDHG', 'ADFF', 'TKJU', 'FTGT'])
+    # when
+    res = board.contains('F')
+    # then
+    expect(res).to(be(True))
+
+
+def test_board_should_verify_vertical_two_chars_words():
+    # given
+    board = Board(['CDHG', 'ADFF', 'TKJU', 'FTGT'])
+    # when
+    res = board.contains('FU')
+    # then
+    expect(res).to(be(True))
+
+
+def test_board_should_verify_horizontal_two_chars_words():
+    # given
+    board = Board(['CDHG', 'ADFF', 'TKJU', 'FTGT'])
+    # when
+    res = board.contains('AD')
+    # then
+    expect(res).to(be(True))
+
+
+def test_board_should_verify_diagonal_two_chars_words():
+    # given
+    board = Board(['CDHG', 'ADFF', 'TKJU', 'FTGT'])
+    # when
+    res = board.contains('AT')
+    # then
+    expect(res).to(be(True))
+
+
+def test_board_should_verify_vertical_plus_horizontal_words():
+    # given
+    board = Board(['CDHG', 'ADFF', 'TKJU', 'FSET'])
+    # when
+    res = board.contains('FUTES')
+    # then
+    expect(res).to(be(True))
